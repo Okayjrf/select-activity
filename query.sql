42d9fc8e31a99a52cbaca90a0993843d898f6fb9
Okayjrf <114513406+Okayjrf@users.noreply.github.com>
2023-02-13 23:18:18 -0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,17 @@
-create index on registration(make);
-create index on registration(model);
-create index on registration(transactiondate);
-create index on registration(electrification);
+-- Here are some querys that you can use in the terminal
+
+SELECT DISTINCT make, model from registration;
+
+SELECT DISTINCT ownertype, model from registration limit 5;
+
+SELECT DISTINCT electrification from registration;
+
+SELECT DISTINCT use, color from registration;
+
+SELECT DISTINCT electrification, state from registration;
+
+-- finds the count of entries in make
+SELECT count(make) from registration;
+
+-- find color for ferrari
+SELECT color from registration where make = 'FERRARI';
